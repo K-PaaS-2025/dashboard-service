0c1eeffeeee4c56042c8094e98ea2fdaf1e0654b
itsmeyji <jungyunji031023@gmail.com>
2025-12-19 16:39:36 +0900

    feat : jwt security settings

---
+++ dev/proj/mirae/mirae/src/main/java/com/yourcode/mirae/auth/entity/RefreshToken.java
@@ -0,0 +1,40 @@
+//package com.yourcode.mirae.auth.entity;
+//
+//import jakarta.persistence.*;
+//import lombok.AccessLevel;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//import java.util.UUID;
+//
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
+//@Getter
+//@Entity
+//public class RefreshToken {
+//    @Id
+//    private UUID uuid;
+//
+//    @OneToOne(fetch = FetchType.LAZY)
+//    @MapsId
+//    @JoinColumn(name = "member_id")
+//    private Member member;
+//    private String refreshToken;
+//    private int reissueCount = 0;
+//
+//    public MemberRefreshToken(Member member, String refreshToken) {
+//        this.member = member;
+//        this.refreshToken = refreshToken;
+//    }
+//
+//    public void updateRefreshToken(String refreshToken) {
+//        this.refreshToken = refreshToken;
+//    }
+//
+//    public boolean validateRefreshToken(String refreshToken) {
+//        return this.refreshToken.equals(refreshToken);
+//    }
+//
+//    public void increaseReissueCount() {
+//        reissueCount++;
+//    }
+//}
